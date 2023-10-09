57a812786fdd266050626b6b308dd8af8e181be5
Tarun <ttarunn0709@gmail.com>
2023-10-09 22:19:58 +0530

    edit card

--- frontend/src/components/TaskCard.js
+++ frontend/src/components/TaskCard.js
@@ -4,12 +4,14 @@ import { useNavigate } from 'react-router-dom'
 const TaskCard = ({ id, title, description }) => {
     const navigate = useNavigate()
   return (
-    <div className='sm:w-80 border-black border p-3 rounded-lg sm:h-96 relative overflow-y-hidden w-[80%] h-[26rem]'>
-        <h1 className='font-bold text-center text-xl'>{title}</h1>
+    <div className='sm:w-80 border-blue-900 border-2 p-3 rounded-lg sm:h-96 overflow-y-hidden w-[80%] h-[26rem] shadow-lg'>
+        <h1 className='font-bold text-center text-xl h-12 text-blue-600'>{title}</h1>
         <p className='font-extralight text-xl text-justify overflow-hidden flex flex-wrap my-2 sm:h-64 h-72'>{description}</p>
-        <Button type='button' className='sm:top-[86%] absolute sm:left-52 top-[88%] left-60' onClick={()=> {
+        <div className='w-full text-center'>
+        <Button type='button' onClick={()=> {
             navigate(`/viewtask/:${id}`)
         }}>View More</Button>
+        </div>
     </div>
   )
 }
